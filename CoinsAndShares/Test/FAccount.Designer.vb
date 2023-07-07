90160cd7109e444d821b9e72ac92b060ab43d88c
Paul <paul@acplus.co.uk>
2023-07-07 17:57:00 +0100

    070723

--- CoinsAndShares/Test/FAccount.Designer.vb
+++ CoinsAndShares/Test/FAccount.Designer.vb
@@ -27,7 +27,6 @@ Namespace Test
         Private Sub InitializeComponent()
             Dim Appearance1 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
             Dim Appearance2 As Infragistics.Win.Appearance = New Infragistics.Win.Appearance()
-            Me.GrdTransactions = New Infragistics.Win.UltraWinGrid.UltraGrid()
             Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
             Me.MaterialLabel1 = New MaterialSkin.Controls.MaterialLabel()
             Me.MLblAccountType = New MaterialSkin.Controls.MaterialLabel()
@@ -45,10 +44,10 @@ Namespace Test
             Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
             Me.TabControl1 = New System.Windows.Forms.TabControl()
             Me.TabPage1 = New System.Windows.Forms.TabPage()
+            Me.GrdTransactions = New Infragistics.Win.UltraWinGrid.UltraGrid()
             Me.TabPage2 = New System.Windows.Forms.TabPage()
             Me.Panel1 = New System.Windows.Forms.Panel()
             Me.MBtnSave = New MaterialSkin.Controls.MaterialButton()
-            CType(Me.GrdTransactions, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.TableLayoutPanel1.SuspendLayout()
             CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SplitContainer1.Panel1.SuspendLayout()
@@ -62,18 +61,9 @@ Namespace Test
             Me.SplitContainer2.SuspendLayout()
             Me.TabControl1.SuspendLayout()
             Me.TabPage1.SuspendLayout()
+            CType(Me.GrdTransactions, System.ComponentModel.ISupportInitialize).BeginInit()
             Me.Panel1.SuspendLayout()
             Me.SuspendLayout()
-            '
-            'GrdTransactions
-            '
-            Appearance1.BackColor = System.Drawing.Color.Silver
-            Me.GrdTransactions.DisplayLayout.Appearance = Appearance1
-            Me.GrdTransactions.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.GrdTransactions.Location = New System.Drawing.Point(0, 0)
-            Me.GrdTransactions.Name = "GrdTransactions"
-            Me.GrdTransactions.Size = New System.Drawing.Size(786, 235)
-            Me.GrdTransactions.TabIndex = 16
             '
             'TableLayoutPanel1
             '
@@ -306,8 +296,8 @@ Namespace Test
             '
             'GrdHoldings
             '
-            Appearance2.BackColor = System.Drawing.Color.Silver
-            Me.GrdHoldings.DisplayLayout.Appearance = Appearance2
+            Appearance1.BackColor = System.Drawing.Color.Silver
+            Me.GrdHoldings.DisplayLayout.Appearance = Appearance1
             Me.GrdHoldings.Dock = System.Windows.Forms.DockStyle.Fill
             Me.GrdHoldings.Location = New System.Drawing.Point(0, 0)
             Me.GrdHoldings.Name = "GrdHoldings"
@@ -342,22 +332,34 @@ Namespace Test
             Me.TabControl1.Name = "TabControl1"
             Me.TabControl1.SelectedIndex = 0
             Me.TabControl1.Size = New System.Drawing.Size(794, 261)
-            Me.TabControl1.TabIndex = 17
+            Me.TabControl1.TabIndex = 18
             '
             'TabPage1
             '
             Me.TabPage1.Controls.Add(Me.GrdTransactions)
             Me.TabPage1.Location = New System.Drawing.Point(4, 22)
             Me.TabPage1.Name = "TabPage1"
+            Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
             Me.TabPage1.Size = New System.Drawing.Size(786, 235)
             Me.TabPage1.TabIndex = 0
             Me.TabPage1.Text = "TRANSACTIONS"
             Me.TabPage1.UseVisualStyleBackColor = True
             '
+            'GrdTransactions
+            '
+            Appearance2.BackColor = System.Drawing.Color.Silver
+            Me.GrdTransactions.DisplayLayout.Appearance = Appearance2
+            Me.GrdTransactions.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.GrdTransactions.Location = New System.Drawing.Point(3, 3)
+            Me.GrdTransactions.Name = "GrdTransactions"
+            Me.GrdTransactions.Size = New System.Drawing.Size(780, 229)
+            Me.GrdTransactions.TabIndex = 16
+            '
             'TabPage2
             '
             Me.TabPage2.Location = New System.Drawing.Point(4, 22)
             Me.TabPage2.Name = "TabPage2"
+            Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
             Me.TabPage2.Size = New System.Drawing.Size(786, 235)
             Me.TabPage2.TabIndex = 1
             Me.TabPage2.Text = "ANALYSIS"
@@ -405,7 +407,6 @@ Namespace Test
             Me.ForeColor = System.Drawing.SystemColors.WindowText
             Me.Name = "FAccount"
             Me.Text = "FAccount"
-            CType(Me.GrdTransactions, System.ComponentModel.ISupportInitialize).EndInit()
             Me.TableLayoutPanel1.ResumeLayout(False)
             Me.TableLayoutPanel1.PerformLayout()
             Me.SplitContainer1.Panel1.ResumeLayout(False)
@@ -420,20 +421,17 @@ Namespace Test
             Me.SplitContainer2.ResumeLayout(False)
             Me.TabControl1.ResumeLayout(False)
             Me.TabPage1.ResumeLayout(False)
+            CType(Me.GrdTransactions, System.ComponentModel.ISupportInitialize).EndInit()
             Me.Panel1.ResumeLayout(False)
             Me.Panel1.PerformLayout()
             Me.ResumeLayout(False)
 
         End Sub
-        Friend WithEvents GrdTransactions As Infragistics.Win.UltraWinGrid.UltraGrid
         Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
         Friend WithEvents SplitContainer1 As SplitContainer
         Friend WithEvents SplitContainer2 As SplitContainer
         Friend WithEvents Panel1 As Panel
         Friend WithEvents GrdHoldings As Infragistics.Win.UltraWinGrid.UltraGrid
-        Friend WithEvents TabControl1 As TabControl
-        Friend WithEvents TabPage1 As TabPage
-        Friend WithEvents TabPage2 As TabPage
         Friend WithEvents MBtnSave As MaterialButton
         Friend WithEvents MaterialLabel1 As MaterialLabel
         Friend WithEvents MLblAccountType As MaterialLabel
@@ -446,6 +444,10 @@ Namespace Test
         Friend WithEvents MCboNetworkId As MaterialComboBox
         Friend WithEvents MSwEnableNetwork As MaterialSwitch
         Friend WithEvents PnlNetworkColour As Panel
+        Friend WithEvents GrdTransactions As Infragistics.Win.UltraWinGrid.UltraGrid
+        Friend WithEvents TabControl1 As TabControl
+        Friend WithEvents TabPage1 As TabPage
+        Friend WithEvents TabPage2 As TabPage
     End Class
 
 End Namespace
