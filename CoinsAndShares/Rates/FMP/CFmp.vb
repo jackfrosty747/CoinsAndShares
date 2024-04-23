a9c24b1d9b6f6c43e249ed209a91a5d99a8698a6
Paul <paul@acplus.co.uk>
2024-04-23 17:45:03 +0100

    230424

--- CoinsAndShares/Rates/FMP/CFmp.vb
+++ CoinsAndShares/Rates/FMP/CFmp.vb
@@ -48,11 +48,37 @@ Namespace Rates.FMP
             Return all
         End Function
 
+        'Friend Function GetNewRates(providerIds As IEnumerable(Of String)) As IEnumerable(Of CRate) Implements IRateProvider.GetNewRates
+
+        '    Dim symbol = providerIds.First
+
+        '    Dim reqUrl As String = $"https://financialmodelingprep.com/api/v3/quote-short/{String.Join(",", providerIds)}?apikey={API_KEY}"
+        '    Dim req As HttpWebRequest = CType(WebRequest.Create(reqUrl), HttpWebRequest)
+        '    req.Method = "GET"
+
+        '    Dim res As HttpWebResponse = CType(req.GetResponse(), HttpWebResponse)
+        '    Dim resStream As Stream = res.GetResponseStream()
+        '    Dim reader As New StreamReader(resStream)
+        '    Dim responseBody As String = reader.ReadToEnd()
+
+        '    res.Close()
+        '    reader.Close()
+
+        '    Dim stockList As List(Of StockInfo) = JsonConvert.DeserializeObject(Of List(Of StockInfo))(responseBody)
+
+        '    Dim rates As New List(Of CRate)
+        '    For Each stockListItem In stockList
+        '        Dim rate = New CRate(stockListItem.Symbol, CDec(stockListItem.Price))
+        '        rates.Add(rate)
+        '    Next
+
+        '    Return rates
+
+        'End Function
+
         Friend Function GetNewRates(providerIds As IEnumerable(Of String)) As IEnumerable(Of CRate) Implements IRateProvider.GetNewRates
 
-            Dim symbol = providerIds.First
-
-            Dim reqUrl As String = $"https://financialmodelingprep.com/api/v3/quote-short/{String.Join(",", providerIds)}?apikey={API_KEY}"
+            Dim reqUrl As String = $"https://financialmodelingprep.com/api/v3/stock/list?apikey={API_KEY}"
             Dim req As HttpWebRequest = CType(WebRequest.Create(reqUrl), HttpWebRequest)
             req.Method = "GET"
 
@@ -64,15 +90,18 @@ Namespace Rates.FMP
             res.Close()
             reader.Close()
 
-            Dim stockList As List(Of StockInfo) = JsonConvert.DeserializeObject(Of List(Of StockInfo))(responseBody)
+            Dim out = New List(Of CRate)
 
-            Dim rates As New List(Of CRate)
-            For Each stockListItem In stockList
-                Dim rate = New CRate(stockListItem.symbol, CDec(stockListItem.price))
-                rates.Add(rate)
+            Dim stockInfoList As List(Of AllStockInfo) = JsonConvert.DeserializeObject(Of List(Of AllStockInfo))(responseBody)
+
+            For Each providerId In providerIds
+                Dim r = stockInfoList.FirstOrDefault(Function(c) c.symbol.ToUpper.Equals(providerId, StringComparison.CurrentCultureIgnoreCase))
+                If r IsNot Nothing Then
+                    out.Add(New CRate(r.symbol, CDec(r.price)))
+                End If
             Next
 
-            Return rates
+            Return out
 
         End Function
 
